efd1218ffd74b4360134d1d6656e3cbfac977143
carolosano <114026092+carolosano@users.noreply.github.com>
2024-11-20 16:27:21 -0300

    agregue Producto.js + packAlternativo.js

---
+++ packAlternativo.js
@@ -0,0 +1,11 @@
+// packAlternativo.js
+class PackAlternativo {
+    constructor(nombre, items) {
+      this.nombre = nombre;
+      this.items = items;
+    }
+  }
+  
+  // Exportamos la clase
+  export default PackAlternativo;
+  
