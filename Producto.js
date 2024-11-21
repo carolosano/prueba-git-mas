7dd5053f2587eafbd5e05b151257681a25cfa445
carolosano <114026092+carolosano@users.noreply.github.com>
2024-11-21 10:14:23 -0300

    Cambie las instancias exportadas que no corresponden

--- Producto.js
+++ Producto.js
@@ -1,20 +1,16 @@
-
 class Producto {
-    constructor(nombrePack, elementos, precio) {
-      this.nombrePack = nombrePack;
-      this.elementos = elementos;
-      this.precio = precio;
-    }
-  
-    mostrarDetalles() {
-      console.log(`Pack: ${this.nombrePack}`);
-      console.log(`Incluye: ${this.elementos.join(", ")}`);
-      console.log(`Precio: $${this.precio}`);
-    }
+  constructor(nombrePack, elementos, precio) {
+    this.nombrePack = nombrePack;
+    this.elementos = elementos;
+    this.precio = precio;
   }
-  
 
-  
-  // Exportamos la clase y las instancias
-  export { Producto, packGrafico, packPublicitario };
-  
+  mostrarDetalles() {
+    console.log(`Pack: ${this.nombrePack}`);
+    console.log(`Incluye: ${this.elementos.join(", ")}`);
+    console.log(`Precio: $${this.precio}`);
+  }
+}
+
+// Exportamos solo la clase Producto
+export { Producto };
