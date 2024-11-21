bcf12a057f2243ce9697011bea3bf2bed3425a1f
carolosano <114026092+carolosano@users.noreply.github.com>
2024-11-21 09:10:31 -0300

    ultimos cambios pendientes resueltos jueves 21.10

--- script.js
+++ script.js
@@ -1,78 +1,18 @@
-//-------------------------------------------------------------------------------------------------------- //
-//document.getElementById("contact-form").addEventListener("submit", function(event) {
-    // prevenir que el formulario realice la acción por defecto (refrescar la página x ej)
-    //event.preventDefault();
 
-    //agarrar los valores de los campos del formulario
-    //const nombre = document.getElementById("first-name").value;
-    //const apellido = document.getElementById("last-name").value;
-    //const empresa = document.getElementById("company").value;
-    //const mensaje = document.getElementById("message").value;
-
-    //objeto con los datos del formulario
-    //const datosContacto = {
-       // nombre: nombre,
-        //apellido: apellido,
-        //empresa: empresa,
-       // mensaje: mensaje
-    //};
-
-    // Almacenar los datos en LocalStorage como JSON
-    //localStorage.setItem("contacto", JSON.stringify(datosContacto));
-
-    // Mostrar el alert
-    //alert("Su mensaje se ha enviado con éxito");
-
-    // Resetear el formulario
-    //document.getElementById("contact-form").reset();
-//});
+  const packGrafico = new Producto(
+    "Gráfico",
+    ["Diseño de Logotipo", "Tarjetas Personales", "Manual de Marca"],
+    150.0
+  );
+  
+  const packPublicitario = new Producto(
+    "Publicitario",
+    ["Diseño de Flyer", "Post para Redes Sociales", "Banner Publicitario"],
+    200.0
+  );
 
 //-------------------------------------------------------------------------------------------------------- //
 
-import { Producto, packGrafico, packPublicitario } from "./Producto.js";
-import PackAlternativo from "./packAlternativo.js";
-
-
-
-//-------------------------------------------------------------------------------------------------------- //
-
-// funcion para manejar la creación del pack
-//function armarPack() {
-    //const checkboxes = document.querySelectorAll('.form-check-input');
-    //const seleccionados = [];
-  
-    //checkboxes.forEach((checkbox, index) => {
-      //if (checkbox.checked) {
-        //seleccionados.push(`Opción ${index + 1}`);
-      //}
-    //}
-    //);
-  
-    //if (seleccionados.length > 0) {
-      //const listaPacks = obtenerPacksDelStorage();
-      //const nuevoPack = new PackAlternativo(`Pack${listaPacks.length + 1}`, seleccionados);
-     //listaPacks.push(nuevoPack);
-      //guardarPacksEnStorage(listaPacks);
-      //alert(`¡Pack ${listaPacks.length} creado con éxito!`);
-    //} else {
-      //alert('Por favor selecciona al menos una opción para crear un pack.');
-    //}
-  //}
-  
-  // Asignar evento al botón "Armar Pack"
-  //document.getElementById('botonArmarPack').addEventListener('click', armarPack);
-  
-  //funciones relacionadas con LocalStorage
-  //function obtenerPacksDelStorage() {
-    //const packs = localStorage.getItem('packs');
-    //return packs ? JSON.parse(packs) : [];
-  //}
-  
-  //function guardarPacksEnStorage(listaPacks) {
-    //localStorage.setItem('packs', JSON.stringify(listaPacks));
-  //}
-  
-//-------------------------------------------------------------------------------------------------------- //
 
   // --------> SIMULACION DEL FORMULARIO. Lo hago mediante promts para no usar DOM   <---------
   function manejarFormulario() {
@@ -104,7 +44,7 @@ function armarPack() {
     let continuar = true;
   
     while (continuar) {
-      // Mostrar menú de opciones
+      // menU de opciones
       const opcion = prompt(
         "Seleccione el número del producto que desea agregar al pack:\n" +
         "1. Producto 01\n" +
